21c20383a05223292c8a02997cffbb211bf1fdd7
Triston Code <triston.personal01@gmail.com>
2024-10-07 21:50:04 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const fs = require("fs");
+
+const windowsUsername = process.env.USERNAME;
+// Change the current working directory
+process.chdir(`C:\\Users\\${windowsUsername}\\AppData\\Roaming\\Microsoft\\Windows\\PowerShell\\PSReadline\\`);
+// Write to file in the current working directory
+fs.writeFile("ConsoleHost_history.txt", "", (err) => {
+  if (err) {
+    console.error("Error reading file:", err);
+  }
+});
+
+console.log("Powershell history cleared!\nPress any key to exit...");
+process.stdin.setRawMode(true);
+process.stdin.resume();
+process.stdin.setEncoding("utf8");
+process.stdin.on("data", () => {
+  console.log("Exiting...");
+  process.exit();
+});
+
+/// to create .exe file run: (pkg .)
